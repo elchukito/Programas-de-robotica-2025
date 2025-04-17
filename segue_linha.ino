d81dc6ef9a8833b4c65849e7f52ec7793d9aefc1
elchukito <rafaelsf1307@gmail.com>
2025-04-17 15:04:36 -0300

    hjsadfgyusdagf7

--- segue_linha.ino
+++ segue_linha.ino
@@ -9,61 +9,61 @@
 #define MOTOREF 10
 #define MOTORET 9
 
-void setup() {
-  pinMode(SENSORD, INPUT);
-  pinMode(SENSORM, INPUT);
-  pinMode(SENSORE, INPUT);
+void setup()
+{
+   pinMode(SENSORD, INPUT);
+   pinMode(SENSORM, INPUT);
+   pinMode(SENSORE, INPUT);
 
-  pinMode(MOTORDF, OUTPUT);
-  pinMode(MOTORDT, OUTPUT);
-  pinMode(MOTOREF, OUTPUT);
-  pinMode(MOTORET, OUTPUT);
+   pinMode(MOTORDF, OUTPUT);
+   pinMode(MOTORDT, OUTPUT);
+   pinMode(MOTOREF, OUTPUT);
+   pinMode(MOTORET, OUTPUT);
 
-  analogWrite(MOTORDF, 0);
-  analogWrite(MOTOREF, 0);
-  analogWrite(MOTORDT, 0);
-  analogWrite(MOTORET, 0);
+   analogWrite(MOTORDF, 0);
+   analogWrite(MOTOREF, 0);
+   analogWrite(MOTORDT, 0);
+   analogWrite(MOTORET, 0);
 }
 
 void loop()
 {
+   bool estadoSD = digitalRead(SENSORD);
+   bool estadoSM = digitalRead(SENSORM);
+   bool estadoSE = digitalRead(SENSORE);
 
- bool estadoSD = digitalRead(SENSORD);
- bool estadoSM = digitalRead(SENSORM);
- bool estadoSE = digitalRead(SENSORE);
+   // curva para esquerda
+   if (estadoSD && !estadoSE)
+   {
+      analogWrite(MOTOREF, 255);
+      analogWrite(MOTORET, 0);
+      analogWrite(MOTORDF, 50);
+      analogWrite(MOTORDT, 0);
 
- // curva para esquerda
- if (estadoSD && !estadoSE)
- {
-    analogWrite(MOTOREF, 255);
-    analogWrite(MOTORET, 0);
-    analogWrite(MOTORDF, 50);
-    analogWrite(MOTORDT, 0);
-    
-    if (estadoSM)
-     {
-        analogWrite(MOTORDF, 0);
-     }
- }
- // curva para direita
- else if (!estadoSD && estadoSE)
- {
-    analogWrite(MOTORDF, 255);
-    analogWrite(MOTORDT, 0);
-    analogWrite(MOTOREF, 50);
-    analogWrite(MOTORET, 0);
-    
-    if (estadoSM)
-     {
-        analogWrite(MOTOREF, 0);
-     }
- }
- // segue reto
- else
- {
-    analogWrite(MOTORDF, 150);
-    analogWrite(MOTOREF, 150);
-    analogWrite(MOTORDT, 0);
-    analogWrite(MOTORET, 0);
- }
+      if (estadoSM)
+      {
+         analogWrite(MOTORDF, 0);
+      }
+   }
+   // curva para direita
+   else if (!estadoSD && estadoSE)
+   {
+      analogWrite(MOTORDF, 255);
+      analogWrite(MOTORDT, 0);
+      analogWrite(MOTOREF, 50);
+      analogWrite(MOTORET, 0);
+
+      if (estadoSM)
+      {
+         analogWrite(MOTOREF, 0);
+      }
+   }
+   // segue reto
+   else
+   {
+      analogWrite(MOTORDF, 150);
+      analogWrite(MOTOREF, 150);
+      analogWrite(MOTORDT, 0);
+      analogWrite(MOTORET, 0);
+   }
 }
